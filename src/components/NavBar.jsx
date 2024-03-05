aaeb12c1ba16ada5ac1b63e7c854a1c50132d4a2
Mike7704 <Michael.cowley2001@gmail.com>
2024-03-05 09:52:17 +0000

    Improved CSS in some places for smaller displays.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,13 +5,13 @@ export default function NavBar() {
   return (
     <div>
       <nav className={navbarStyles.container}>
-        <Link className="button" href="/">
+        <Link className={`button ${navbarStyles.nav_button}`} href="/">
           Home
         </Link>
-        <Link className="button" href="/games?category=All">
+        <Link className={`button ${navbarStyles.nav_button}`} href="/games?category=All">
           Games
         </Link>
-        <Link className="button" href="/addGame">
+        <Link className={`button ${navbarStyles.nav_button}`} href="/addGame">
           Add Game
         </Link>
       </nav>
